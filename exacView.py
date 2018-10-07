5734266e3f10d5ffaee6a522c87a5c2150dd3ebb
Adith Arun <aarun3@jhu.edu>
2018-10-07 02:02:29 -0400

    Row Select exacView.py
    Logic to figure out which rows to select from input file

--- exacView.py
+++ exacView.py
@@ -22,7 +22,7 @@ formatSheet = formatFile.add_worksheet()
 
 #figure out which mutation types to consider
 mutations = list(sys.argv[2])
-dict_mutations = {'1: all', '2: missense', '3: LoF', '4: frameshift', '5: 5' UTR', '6: 3' UTR', '7: synonymous', '8: splice', '9: intron'}def mutationMap(dict_mutations, mutations):
+dict_mutations = {'1: all', '2: missense', '3: non coding transcript exon', '4: frameshift', '5: 5' UTR', '6: 3' UTR', '7: synonymous', '8: splice', '9: intron'}def mutationMap(dict_mutations, mutations):
     for item in mutations:
         if item =='1':
         	mutations.append(dict_mutations[item])
@@ -36,3 +36,13 @@ mutations = [x for x in mutations if not (x.isdigit())]
 
 #columns not to get
 colList = [0, 1, 2, 3, 4, 5, 8, 10]
+
+#rows to get (frequency greater than 1 and satisfies mutations to consider criteria)
+
+rowList = []
+for x in range(sheet.nrows):
+    try: 
+        if float(sheet.cell_value(x, 11)) > 1.0:
+            rowList.append(x)
+    except: 
+        pass
