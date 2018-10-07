c9c08ec62a608bc9f0c4368df6977a6d2a7b447a
Adith Arun <aarun3@jhu.edu>
2018-10-07 01:45:47 -0400

    exacView.py updates
    output file specification and processing input arguments

--- exacView.py
+++ exacView.py
@@ -19,3 +19,20 @@ def findLast(path, character):
 
 formatFile = xlsxwriter.Workbook(os.getcwd() + "/FORMATTED" + data[findLast(data, '/') + 1: ])
 formatSheet = formatFile.add_worksheet()
+
+#figure out which mutation types to consider
+mutations = list(sys.argv[2])
+dict_mutations = {'1: all', '2: missense', '3: LoF', '4: frameshift', '5: 5' UTR', '6: 3' UTR', '7: synonymous', '8: splice', '9: intron'}def mutationMap(dict_mutations, mutations):
+    for item in mutations:
+        if item =='1':
+        	mutations.append(dict_mutations[item])
+        	return
+        if item in dict_mutations.keys():
+            mutations.append(dict_mutations[item])
+
+            
+mutationMap(dict_mutations, mutations)
+mutations = [x for x in mutations if not (x.isdigit())]
+
+#columns not to get
+colList = [0, 1, 2, 3, 4, 5, 8, 10]
